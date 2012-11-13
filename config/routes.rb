8f5ef63dcdb8374f76fac2df51a25a8eefd49f34
Geoff Harcourt <geoff.harcourt@gmail.com>
2012-11-13 12:35:54 -0500

    Restricted routes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Fivetool::Application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
 
-  resources :blog_entries
+  resources :blog_entries, only: [:index, :show]
 
-  resources :contact_messages
+  resources :contact_messages, only: [:create]
 
   get '/home', to: 'home#index', as: 'home'
 
