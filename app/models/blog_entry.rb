6ea7b7b1d80a20be4b503d1b1ac424a69ac0d348
Geoff Harcourt <geoff.harcourt@gmail.com>
2013-02-01 09:15:53 -0500

    BlogEntry#publish and #unpublish.

--- app/models/blog_entry.rb
+++ app/models/blog_entry.rb
@@ -1,13 +1,22 @@
 class BlogEntry < ActiveRecord::Base
-  
+
   attr_accessible :author, :content, :published_at, :title
 
   validates_presence_of :author, :content, :title
 
   scope :published, -> { where('published_at IS NOT NULL') }
 
+  def publish
+    self.published_at ||= Time.now
+    save!
+  end
+
   def to_s
     title
+  end
+
+  def unpublish
+    update_attributes!(published_at: nil)
   end
 
 end
