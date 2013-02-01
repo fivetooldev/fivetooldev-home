6fa5e546c5c4e618f42b9d17cc735a3fcab39123
Geoff Harcourt <geoff.harcourt@gmail.com>
2013-02-01 09:17:32 -0500

    Reordered blog entry specs.

--- spec/models/blog_entry_spec.rb
+++ spec/models/blog_entry_spec.rb
@@ -1,41 +1,59 @@
 require 'spec_helper'
 
 describe BlogEntry, :blog_entry, :model do
-  before(:each) { Timecop.freeze(Time.new(2013,2,1,13,35)) }
-
   it { should validate_presence_of(:author) }
   it { should validate_presence_of(:content) }
   it { should_not validate_presence_of(:published_at) }
   it { should validate_presence_of(:title) }
 
-  describe '#publish' do
-    context 'with an unpublished article' do
-      let(:blog_entry) { create(:blog_entry, published_at: nil) }
+  describe 'publish and unpublish behavior' do
+    before(:each) { Timecop.freeze(Time.new(2013,2,1,13,35)) }
 
-      it 'sets the published_at time' do
-        blog_entry.publish
+    describe '#publish' do
+      context 'with an unpublished article' do
+        let(:blog_entry) { create(:blog_entry, published_at: nil) }
 
-        blog_entry.published_at.should eq(Time.new(2013,2,1,13,35))
+        it 'sets the published_at time' do
+          blog_entry.publish
+
+          blog_entry.published_at.should eq(Time.new(2013,2,1,13,35))
+        end
+
+        it 'saves the model' do
+          blog_entry.publish
+
+          blog_entry.changed?.should be_false
+        end
       end
 
-      it 'saves the model' do
-        blog_entry.publish
+      context 'with a published article' do
+        let(:blog_entry) { create(:blog_entry, published_at: Time.new(2013,1,1)) }
 
-        blog_entry.changed?.should be_false
+        it 'sets the published_at time' do
+          blog_entry.publish
+
+          blog_entry.published_at.should eq(Time.new(2013,1,1))
+        end
+
+        it 'saves the model' do
+          blog_entry.publish
+
+          blog_entry.changed?.should be_false
+        end
       end
     end
 
-    context 'with a published article' do
-      let(:blog_entry) { create(:blog_entry, published_at: Time.new(2013,1,1)) }
+    describe '#unpublish' do
+      let(:blog_entry) { create(:blog_entry, published_at: Time.now) }
 
-      it 'sets the published_at time' do
-        blog_entry.publish
+      it 'sets the published_at time to nil' do
+        blog_entry.unpublish
 
-        blog_entry.published_at.should eq(Time.new(2013,1,1))
+        blog_entry.published_at.should be_nil
       end
 
       it 'saves the model' do
-        blog_entry.publish
+        blog_entry.unpublish
 
         blog_entry.changed?.should be_false
       end
@@ -47,22 +65,6 @@ describe BlogEntry, :blog_entry, :model do
       blog_entry  = build(:blog_entry, title: 'A meaningful title')
 
       blog_entry.to_s.should eq('A meaningful title')
-    end
-  end
-
-  describe '#unpublish' do
-    let(:blog_entry) { create(:blog_entry, published_at: Time.now) }
-
-    it 'sets the published_at time to nil' do
-      blog_entry.unpublish
-
-      blog_entry.published_at.should be_nil
-    end
-
-    it 'saves the model' do
-      blog_entry.unpublish
-
-      blog_entry.changed?.should be_false
     end
   end
 
