0a9590464af1654b3cba96589018031dc04e0ec3
Geoff Harcourt <geoff.harcourt@gmail.com>
2013-02-12 14:11:53 -0500

    Redirect to tumblr.

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -3,16 +3,11 @@ class BlogController < ApplicationController
   layout 'blog'
 
   def index
-    @blog_entries = BlogEntry.order('published_at DESC').published
-
-    respond_to do |format|
-      format.html { render :index }
-      format.rss { render layout: false }
-    end
+    redirect_to 'http://blog.fivetool.io'
   end
 
   def show
-    @blog_entry = BlogEntry.published.find(params[:id])
+    redirect_to 'http://blog.fivetool.io'
   end
 
 end
