d0819d86f731f91862f015205e66d26376232a55
Geoff Harcourt <geoff.harcourt@gmail.com>
2013-03-20 10:22:09 -0400

    Move secret token to environment variable.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Fivetool::Application.config.secret_token = 'f0e37f77768743912bcabe77c20eb097e4982c091a332c29c68ddd515c14511d0241d5cb23e6a33940f6ec13bb9091dd55cd198e597ed89ece1ac266b41f7179'
+Fivetool::Application.config.secret_token = ENV['RAILS_SECRET_TOKEN']
