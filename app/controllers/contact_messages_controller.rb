508c934500a36bd467909867cb2e9e7f1a39e2e0
Geoff Harcourt <geoff.harcourt@gmail.com>
2013-04-19 11:57:57 -0400

    Refactored controller.

--- app/controllers/contact_messages_controller.rb
+++ app/controllers/contact_messages_controller.rb
@@ -1,23 +1,41 @@
+##
+# Controller for contact us messages.
+##
 class ContactMessagesController < ApplicationController
 
   def create
-    @contact_message = ContactMessage.new(params[:contact_message])
+    build_contact_message
 
     if @contact_message.save
       deliver_notification_email
 
-      notice = 'Thanks for contacting us. We will be in touch shortly.'
+      set_success_flash_notice
     else
-      notice =  'There was a problem with your submission. Please try again or email us at info@fivetool.io'
+      set_error_flash_message
     end
 
-    redirect_to root_path, notice: notice
+    redirect_to root_path
   end
 
   private
 
+  def build_contact_message
+    @contact_message = ContactMessage.new(params[:contact_message])
+  end
+
   def deliver_notification_email
     ContactMailer.signup_confirmation(@contact_message).deliver
+  end
+
+  def set_success_flash_notice
+    flash[:notice] = 'Thanks for contacting us. We will be in touch shortly.'
+  end
+
+  def set_error_flash_message
+    flash[:error] = <<-heredoc
+      There was a problem with your submission. Please try again or email
+      us at info@fivetool.io
+    heredoc
   end
 
 end
