4cf79432f93dacd592c2c087a2f01e259193ca98
Geoff Harcourt <geoff.harcourt@gmail.com>
2013-04-19 18:24:00 -0400

    Add fake secret for testing.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Fivetool::Application.config.secret_token = ENV['RAILS_SECRET_TOKEN']
+if Rails.env.test?
+  Fivetool::Application.config.secret_token = '23cf389ccb9ecc2adc9d88d25154ff7504f2c1c5dca1c9d8a9ad59eb269e4e494e61fa034d1005555bc33deb633c63321e3e9febfe94ecaa2407578cb82cfeb2'
+else
+  Fivetool::Application.config.secret_token = ENV['RAILS_SECRET_TOKEN']
+end
