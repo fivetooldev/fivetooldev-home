aca3ee4b7448c7f2e2ef70f78b56b113edde0b79
Geoff Harcourt <geoff.harcourt@gmail.com>
2013-04-19 14:54:58 -0400

    Removed unnecessary configuration line.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -9,7 +9,6 @@ class ContactMailer < ActionMailer::Base
     @contact_message = contact_message
     mail to: contact_message.email_address,
       bcc: 'geoff@fivetool.io',
-      subject: "Thanks for your message",
-      content_type: "multipart/alternative"
+      subject: "Thanks for your message"
   end
 end
