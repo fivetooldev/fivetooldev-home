f2f7bda9f1f83bc084b71125a1165f468d28afc8
Geoff Harcourt <geoff.harcourt@gmail.com>
2013-05-05 16:46:37 -0400

    Add email_validator, use on ContactMessage#email

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 ruby "2.0.0"
 
+gem 'email_validator'
 gem 'haml-rails'
 gem 'jquery-rails'
 gem 'pg'
--- app/models/contact_message.rb
+++ app/models/contact_message.rb
@@ -5,6 +5,7 @@ class ContactMessage < ActiveRecord::Base
 
   attr_accessible :email_address, :message, :name
 
-  validates_presence_of :email_address, :name
+  validates :email_address, presence: true, email: true
+  validates_presence_of :name
 
 end
