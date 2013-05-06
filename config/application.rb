726e3006c2a3178084b472c80945331c503c9bb6
Geoff Harcourt <geoff.harcourt@gmail.com>
2013-05-06 08:28:53 -0400

    Application.rb: simplify bundler declaration, remove unneeded assets.enabled line

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
-end
+Bundler.require(:default, Rails.env)
 
 module Fivetool
   class Application < Rails::Application
@@ -27,9 +22,6 @@ module Fivetool
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
-
-    # Enable the asset pipeline
-    config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
